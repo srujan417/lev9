222141dbef1945342560244e64005205b40c19bb
Bhoomika146 <116084949+Bhoomika146@users.noreply.github.com>
2023-01-14 13:38:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const path = require("path");
 const { Todo } = require("./models");
 // eslint-disable-next-line no-unused-vars
 const todo = require("./models/todo");
+// eslint-disable-next-line no-undef
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser("shh! some secret string"));
@@ -36,7 +37,6 @@ app.get("/", async (request, response) => {
 });
 
 app.get("/todos", async (request, response) => {
-  // defining route to displaying message
   console.log("Todo list");
   try {
     const todoslist = await Todo.findAll();
@@ -57,13 +57,12 @@ app.get("/todos/:id", async function (request, response) {
 });
 
 app.post("/todos", async (request, response) => {
-  console.log("creating new todo", request.body);
+  console.log("creating a new todo", request.body);
   try {
-    // eslint-disable-next-line no-unused-vars
     await Todo.addTodo({
       title: request.body.title,
       dueDate: request.body.dueDate,
-      commpleted: false,
+      completed: false,
     });
     return response.redirect("/");
   } catch (error) {
@@ -71,9 +70,9 @@ app.post("/todos", async (request, response) => {
     return response.status(422).json(error);
   }
 });
-//PUT https://mytodoapp.com/todos/123/markAscomplete
+
 app.put("/todos/:id", async (request, response) => {
-  console.log("Mark Todo as completed:", request.params.id);
+  console.log("Mark a Todo as completed:", request.params.id);
   const todo = await Todo.findByPk(request.params.id);
   try {
     const updatedtodo = await todo.setCompletionStatus(request.body.completed);
@@ -84,7 +83,7 @@ app.put("/todos/:id", async (request, response) => {
   }
 });
 app.delete("/todos/:id", async (request, response) => {
-  console.log("delete a todo with ID:", request.params.id);
+  console.log("Delete a todo with ID:", request.params.id);
   try {
     await Todo.remove(request.params.id);
     return response.json({ success: true });
